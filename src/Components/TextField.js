0326580d85538359398602dd28b2bcd97316332a
BorgeVijay07 <borgevijay11@gmail.com>
2021-08-20 14:40:01 +0530

    Added more buttons and functionality in website.

--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 
 export default function TextField(props) {
 
-    const [text, setText] = useState('Enter Text Here');
+    const [text, setText] = useState('');
     const upClickHandler = () => {
         let newText = text.toUpperCase();
         setText(newText);
@@ -10,14 +10,44 @@ export default function TextField(props) {
     const changeHandler = (event) => {
         setText(event.target.value);
     }
+    const loClickHandler = () => {
+        let newText = text.toLowerCase();
+        setText(newText);
+    }
+    const clClickHandler = () => {
+        let newText = '';
+        setText(newText);
+    }
+    const coClickHandler = () => {
+        var text = document.getElementById("textbox");
+        text.select();
+        navigator.clipboard.writeText(text.value);
+    }
+    const rsClickHandler = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(' '));
+    }
 
     return (
         <>
-            <h1>{props.heading}</h1>
-            <div className="mb-3">
-                <textarea className="form-control" id="textbox" rows="8" value={text} onChange={changeHandler}></textarea>
+            <div className="container">
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="textbox" rows="8" value={text} onChange={changeHandler}></textarea>
+                </div>
+                <button className="btn btn-primary mx-2" onClick={upClickHandler}>Convert to Uppercase</button>
+                <button className="btn btn-primary mx-2" onClick={loClickHandler}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-2" onClick={rsClickHandler}>Remove Extra Spaces</button>
+                <button className="btn btn-primary mx-2" onClick={coClickHandler}>Copy to Clipboard</button>
+                <button className="btn btn-primary mx-2" onClick={clClickHandler}>Clear Text</button>
             </div>
-            <button className="btn btn-primary" onClick={upClickHandler}>Convert to Uppercase</button>
+            <div className="container my-3">
+                <h2>Your Text Summary:</h2>
+                <p><b>{text.split(" ").length}</b> Words and <b>{text.length}</b> Characters</p>
+                <p><b>{0.008 * text.split(" ").length}</b> Minutes Read</p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
         </>
     )
 }
