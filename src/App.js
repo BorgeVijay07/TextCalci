10f1dfb3b4b00903274afbf7b7bf679c02a446d6
BorgeVijay07 <borgevijay11@gmail.com>
2021-08-21 13:59:50 +0530

    Added dark mode functionality.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import './App.css';
+// import About from './Components/About';
 import Navbar from './Components/Navbar';
 import TextField from './Components/TextField';
+import React, { useState } from 'react'
 
 function App() {
+
+  const [mode, setMode] = useState('light')
+  const toggleMode = () => {
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#000a44';
+    }else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-      <Navbar title="TextCalci" aboutname="About"/>
+      <Navbar title="TextCalci" aboutname="About" mode={mode} toggleMode={toggleMode}/>
       <div className="container my-3">
-        <TextField heading="Please Enter Your Text Here"/>
+        <TextField heading="Please Enter Your Text Here" mode={mode}/>
+        {/* <About/> */}
       </div>
     </>
   );
--- src/Components/TextField.js
+++ src/Components/TextField.js
@@ -30,10 +30,10 @@ export default function TextField(props) {
 
     return (
         <>
-            <div className="container">
+            <div className="container" style={{color: props.mode==='light'?'black':'white'}}>
                 <h1>{props.heading}</h1>
                 <div className="mb-3">
-                    <textarea className="form-control" id="textbox" rows="8" value={text} onChange={changeHandler}></textarea>
+                    <textarea className="form-control" id="textbox" style={{backgroundColor: props.mode==='light'?'white':'grey', color: props.mode==='light'?'black':'white'}} rows="8" value={text} onChange={changeHandler}></textarea>
                 </div>
                 <button className="btn btn-primary mx-2" onClick={upClickHandler}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-2" onClick={loClickHandler}>Convert to Lowercase</button>
@@ -41,12 +41,12 @@ export default function TextField(props) {
                 <button className="btn btn-primary mx-2" onClick={coClickHandler}>Copy to Clipboard</button>
                 <button className="btn btn-primary mx-2" onClick={clClickHandler}>Clear Text</button>
             </div>
-            <div className="container my-3">
+            <div className="container my-3" style={{color: props.mode==='light'?'black':'white'}}>
                 <h2>Your Text Summary:</h2>
                 <p><b>{text.split(" ").length}</b> Words and <b>{text.length}</b> Characters</p>
                 <p><b>{0.008 * text.split(" ").length}</b> Minutes Read</p>
                 <h2>Preview</h2>
-                <p>{text}</p>
+                <p>{text.length>0?text:'Enter Something To Prieviw It Here'}</p>
             </div>
         </>
     )
